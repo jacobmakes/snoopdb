f11e68e23c08bb460dbd8d9dd308e0a66d45174c
Jacob T <82041930+J-creative@users.noreply.github.com>
2022-08-14 16:44:58 +0100

    read table from file

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import * as os from 'os'
 import { Buffer } from 'buffer'
 import { stringify } from 'querystring'
 import { EventEmitter } from 'stream'
+import { resolve } from 'path'
 
 console.time()
 
@@ -13,9 +14,19 @@ function readBinary(path: rP[0], options?: rP[1]) {
     const str = fs.createReadStream(path, options)
     // const str= fs.createReadStream('myBinaryFile'); //B
     let chunks = ''
-    str.on('data', (chunk) => (chunks += chunk.toString('utf-8'))) //A & B
+    str.on('data', chunk => (chunks += chunk.toString('utf-8'))) //A & B
     str.on('end', () => console.log('ll', chunks, chunks.length)) //A 5.609ms
 }
+
+const cutNull = (string: string) => {
+    let i = 0
+    while (i < string.length) {
+        if (string[i] === '\x00') return string.substring(0, i)
+        i++
+    }
+    return string
+}
+
 function calculateRowSize(schema: schema) {
     let bytes = 0
     for (const col of schema) {
@@ -47,8 +58,14 @@ interface queueItem {
     data?: Buffer
     symbol?: Symbol
 }
+interface tableConfig {
+    overwrite?: boolean
+}
 
 class Table extends EventEmitter {
+    private name: string
+    private columns: schema
+    private config: any
     protected start: number
     protected rowSize: number
     private queue: queueItem[]
@@ -56,19 +73,18 @@ class Table extends EventEmitter {
     private writeStream
     private rows: number
 
-    constructor(
-        readonly path: string,
-        readonly name: string,
-        private columns?: schema,
-        private config?: any
-    ) {
+    constructor(readonly path: string) {
         super()
         this.queue = []
         this.locked = false
-        //check if table exists
-
-        //if table exist throw error if columns provided
-
+        this.writeStream = fs.createWriteStream(this.path, { encoding: 'binary', flags: 'a' })
+    }
+    public createTable(name: string, columns: schema, config?: tableConfig) {
+        if (fs.existsSync(this.path) && config?.overwrite !== true)
+            throw 'table already exists set config.overwrite to true to overwrite'
+        this.name = name
+        this.columns = columns
+        this.config = config
         //create table
         /* Table form bytes, reason */
         // 4 location data starts
@@ -79,21 +95,21 @@ class Table extends EventEmitter {
         if (!columns) {
             throw 'Table Schema undefined'
         }
-        columns.forEach((col) => {
+        columns.forEach(col => {
             if (!colTypes.includes(col[1])) {
                 throw `invalid type ${col[1]} acceptable types: ${colTypes.join('","')}`
             }
         })
 
-        const schemaLength = Buffer.from(JSON.stringify(schema)).length
-        const START = 60
+        const schemaLength = Buffer.from(JSON.stringify(columns)).length
+        const START = schemaLength + 12 // + add options hedaers to size
         const VERSION = 1
         const SCHEMA_END = schemaLength + 12
         const headers = Buffer.alloc(START)
         headers.writeInt32LE(START)
         headers.writeInt32LE(VERSION, 4)
         headers.writeInt32LE(SCHEMA_END, 8)
-        headers.write(JSON.stringify(schema), 12)
+        headers.write(JSON.stringify(columns), 12)
         // const wstream = fs.createWriteStream(this.path, { encoding: 'binary' })
         // wstream.write(headers, () =>
         // )
@@ -104,11 +120,50 @@ class Table extends EventEmitter {
         //     end: SCHEMA_END - 1,
         // })
         this.start = START
+        console.log(headers)
         //@ts-ignore
         this.rowSize = calculateRowSize(this.columns)
-        console.log(headers)
         this.rows = 0
-        this.writeStream = fs.createWriteStream(this.path, { encoding: 'binary', flags: 'a' })
+        console.log('created table')
+    }
+    public async getTable() {
+        //check if table exists
+        if (!fs.existsSync(this.path)) throw 'table does not exist'
+
+        return new Promise((resolve, reject) => {
+            console.log('this.path', this.path)
+            const readMeta = fs.createReadStream(this.path, { end: 12 })
+            readMeta.on('data', (chunk: Buffer) => {
+                this.start = chunk.readInt32LE()
+                const schemaEnd = chunk.readInt32LE(8)
+                console.log('schemaEnd', schemaEnd)
+
+                const readSchema = fs.createReadStream(this.path, {
+                    start: 12,
+                    end: schemaEnd - 1,
+                    highWaterMark: schemaEnd,
+                })
+                readSchema.on('data', (chunk: Buffer) => {
+                    console.log('cols', chunk.toString())
+                    const cols = JSON.parse(chunk.toString())
+                    this.columns = cols
+                    this.rowSize = calculateRowSize(this.columns)
+                    const { size } = fs.statSync(this.path)
+                    const rowCount = (size - this.start) / this.rowSize
+                    if (!Number.isInteger(rowCount)) throw 'non integer rowCount'
+                    this.rows = rowCount
+                    resolve(cols)
+                })
+            })
+        })
+    }
+    public async getRowCount() {
+        return new Promise(async res =>
+            res(((await fs.promises.stat(this.path)).size - this.start) / this.rowSize)
+        )
+    }
+    public async getRowCountSync() {
+        return (fs.statSync(this.path).size - this.start) / this.rowSize
     }
     private add(item: queueItem) {
         this.locked ? this.queue.push(item) : this.process(item)
@@ -184,7 +239,7 @@ class Table extends EventEmitter {
                 throw 'row too large'
             }
             const symbol = Symbol()
-            this.on(symbol, (id) => {
+            this.once(symbol, id => {
                 // console.log('listening', id)
                 // console.timeLog()
                 const out: { [key: string]: any } = { id }
@@ -216,7 +271,9 @@ class Table extends EventEmitter {
                 for (let i = 0; i < cols?.length; i++) {
                     switch (cols[i][1]) {
                         case 'string':
-                            out[cols[i][0]] = chunk.toString('utf8', offset, offset + cols[i][2])
+                            out[cols[i][0]] = cutNull(
+                                chunk.toString('utf8', offset, offset + cols[i][2])
+                            )
                             offset += cols[i][2]
                             break
                         case 'int':
@@ -245,7 +302,7 @@ class Table extends EventEmitter {
 }
 
 //cars
-const schema: schema = [
+const schema1: schema = [
     //id is auto
     ['model', 'string', 8],
     ['produced', 'int', 4], // or allow user to enter a max number and calc eg. 1_000_000 => 3 because 3 bye max over 1mil
@@ -253,7 +310,8 @@ const schema: schema = [
 ]
 
 async function main() {
-    const cars = new Table('samples/cars.db', 'cars', schema)
+    const cars = new Table('samples/cars.db')
+    cars.createTable('cars', schema1, { overwrite: true })
     const id = await cars.push(['foffo', 435])
     console.log('id', id)
 
@@ -263,7 +321,7 @@ async function main() {
     await cars.push(['b', 43])
     await cars.push(['triumpth', 380])
     console.log('id2', id2)
-    for (let i = 0; i < 600; i++) {
+    for (let i = 0; i < 600000; i++) {
         let r = randomString()
         await cars.push([r, Math.floor(Math.random() * 1000000)])
     }
@@ -274,7 +332,17 @@ async function main() {
 
     console.timeLog()
 }
-//main()
+// main()
+
+async function read() {
+    const books = new Table('samples/cars.db') //6million
+    // books.createTable('books', schema2, { overwrite: true })
+    await books.getTable()
+    console.log(await books.getRowCount())
+    const dat = await books.read(5555555)
+    console.log('dat', dat)
+}
+read()
 
 function randomString() {
     return Math.random().toString(36).slice(2, 7)
@@ -290,10 +358,17 @@ const schema2: schema = [
 ]
 
 async function main2() {
-    const books = new Table('samples/books.db', 'books', schema2)
-    const data = await books.push(['harry pott', 'jk rowlingS', 1998])
-    console.log('data', data)
-    const dat = await books.read(1)
-    console.log('dat', dat)
+    try {
+        const books = new Table('samples/books.db')
+        books.createTable('books', schema2, { overwrite: true })
+        // await books.getTable()
+        const data = await books.push(['harry potter', 'jk rowling', 1998])
+        await books.push(['da vincis co', 'dan brown', 2004])
+        // console.log('data', data)
+        const dat = await books.read(1)
+        console.log('dat', dat)
+    } catch (error) {
+        console.log(error)
+    }
 }
-main2()
+// main2()
